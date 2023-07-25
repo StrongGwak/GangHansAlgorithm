654ac5af7abdd7f54387462b2554a766a27dc87a
GangHan <40186760+StrongGwak@users.noreply.github.com>
2023-07-25 10:13:57 +0900

    Update 호텔 대실.java

--- 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.java
@@ -6,20 +6,30 @@ class Solution {
         // 빠른 시간순으로 정렬
         sort(book_time);
         int[][] times;
+        
         // String to Integer 변환
         times = tran(book_time);
+        
+        // 최소한의 객실 구하기
         int answer = reservation(times);
         return answer;
     }
     
     public int reservation(int[][] times) {
+        // 방 ArrayList
         ArrayList<Integer> room = new ArrayList<Integer>();
+        // 첫 손님 방의 퇴실 시간 할당
         room.add(times[0][1]);
+        // 두번째 손님부터 반복
         for(int i = 1; i < times.length; i++) {
+            // 예약된 방의 수 만큼 반복
             for(int j = 0; j < room.size(); j++) {
+                // 예약된 방의 퇴실 시간 + 청소시간 보다 크거나 같으면 
+                // 다음 손님의 퇴실 시간을 그 방에 할당 후 반복 종료
                 if(room.get(j) <= times[i][0]) {
                     room.set(j,times[i][1]);
                     break;
+                // 마지막 방까지 확인 했으면 새로운 방에 할당
                 } else if(j == room.size() - 1) {
                     room.add(times[i][1]);
                     break;
