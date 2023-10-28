9cafc440020d602185cc0521a8ba370305ffff87
GangHan <40186760+StrongGwak@users.noreply.github.com>
2023-10-28 13:45:14 +0900

    [level unrated] Title: 조건 문자열, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) {
+    int answer = 0;
+    if(eq.compare("=") == 0){
+        if (ineq.compare(">") == 0) {
+            if(n >= m) {
+                answer = 1;
+            }
+        }
+        else
+        {
+            if(n <= m) {
+                answer = 1;
+            }
+        }
+    }
+    else 
+    {
+        if (ineq.compare(">") == 0) {
+            if(n > m) {
+                answer = 1;
+            }
+        }
+        else
+        {
+            if(n < m) {
+                answer = 1;
+            }
+        }
+    }
+    
+    return answer;
+}
