45064e0be437e6cfa62560456369e660a632d314
GangHan <40186760+StrongGwak@users.noreply.github.com>
2024-01-10 18:43:59 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool desc(int a, int b){ 
+  return a > b; 
+} 
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+    sort(score.begin(), score.end(), desc);
+    for (int i = 0; i < score.size(); i++) {
+        if ((i + 1) % m == 0) {
+            answer += score[i] * m;
+        }
+    }
+    return answer;
+}
