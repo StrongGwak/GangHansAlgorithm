1e0c36a24d04bf61662f019d3cdd4af2ee179d66
GangHan <40186760+StrongGwak@users.noreply.github.com>
2024-03-26 11:52:04 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+    int total = brown + yellow;
+    int x = 3;
+    int y = 3;
+    
+    while (true) {
+        if(x >= y && (x - 2) * (y - 2) == yellow) {
+            answer.push_back(x);
+            answer.push_back(y);
+            return answer;
+        }
+        x++;
+        if(total % x == 0) {
+            y = total / x;
+        }
+    }
+    
+    return answer;
+}
