3f8995bc961211f6a1c2ba0f4b6339dbb791ffa3
GangHan <40186760+StrongGwak@users.noreply.github.com>
2024-03-28 12:47:54 +0900

    Update 택배상자.cpp

--- 프로그래머스/2/131704. 택배상자/택배상자.cpp
+++ 프로그래머스/2/131704. 택배상자/택배상자.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <vector>
 #include <stack>
-#include <iostream>
 
 using namespace std;
 
@@ -10,12 +9,15 @@ int solution(vector<int> order) {
     int o = 0;
     stack<int> main;
     stack<int> sub;
+    // 기존 컨테이너 벨트 스택에 상자 정보 삽입
     for (int i = order.size(); i >= 1; i--) {
         main.push(i);
     }
     
     while (!main.empty() || !sub.empty()) {
+        // 트럭에 실었는지 여부
         bool flag = false;
+        // 기존 컨테이너 벨트 체크
         if(!main.empty() && o < order.size() 
            && main.top() == order[o]) {
             main.pop();
@@ -23,6 +25,7 @@ int solution(vector<int> order) {
             o++;
             flag = true;
         }
+        // 보조 컨테이너 벨트 체크
         if(!sub.empty() && o < order.size() 
            && sub.top() == order[o]) {
             sub.pop();
@@ -30,13 +33,14 @@ int solution(vector<int> order) {
             o++;
             flag = true;
         }
+        // 싣지 못했다면 (기존 -> 보조) 상자 이동
         if (!main.empty() && !flag) {
             sub.push(main.top());
             main.pop();
         } else if (main.empty() && !flag) {
+            // 기존 컨테이너 벨트에 상자가 없고 트럭에 싣지도 못했다면 종료
             break;
         }
-        
     }
     return answer;
 }
