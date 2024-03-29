8743dbe0d1f2064e1b4e8b982266de65ed2a5eb7
GangHan <40186760+StrongGwak@users.noreply.github.com>
2024-03-29 11:49:02 +0900

    [level 2] Title: 점 찍기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+long long solution(int k, int d) {
+    long long answer = 0;
+    long long dd = pow(d, 2);
+    for (int i = 0; i <= d; i += k) {
+        long long yy = dd - pow(i, 2);
+        answer += ((int)sqrt(yy) / k) + 1;
+    }
+    return answer;
+}
