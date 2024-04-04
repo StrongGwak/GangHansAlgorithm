919bb83cf0d03e00350759f2e80e9d11d8f723a6
GangHan <40186760+StrongGwak@users.noreply.github.com>
2024-04-04 09:26:32 +0900

    [level 2] Title: 구명보트, Time: 1.39 ms, Memory: 4.63 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    vector<bool> escape;
+    sort(people.begin(), people.end());
+    int front = 0;
+    int back = people.size() - 1;
+    while (front <= back) {
+        if (people[front] + people[back] <= limit) {
+            answer++;
+            front++;
+            back--;
+        } else {
+            back--;
+            answer++;
+        }
+    }
+    
+    return answer;
+}
