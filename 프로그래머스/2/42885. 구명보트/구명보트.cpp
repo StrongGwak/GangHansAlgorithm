f070e3c84579b9fccbf2ddcab10524d0a1a974fb
GangHan <40186760+StrongGwak@users.noreply.github.com>
2024-04-04 09:30:15 +0900

    Update 구명보트.cpp

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -8,8 +8,11 @@ int solution(vector<int> people, int limit) {
     int answer = 0;
     vector<bool> escape;
     sort(people.begin(), people.end());
+    // 가벼운 사람
     int front = 0;
+    // 무거운 사람
     int back = people.size() - 1;
+    // 투포인터로 만날때까지 반복
     while (front <= back) {
         if (people[front] + people[back] <= limit) {
             answer++;
