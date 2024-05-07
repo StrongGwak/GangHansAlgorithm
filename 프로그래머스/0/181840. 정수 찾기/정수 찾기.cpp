665d96bdb7378e9477c834214075d4d68fac2446
GangHan <40186760+StrongGwak@users.noreply.github.com>
2024-05-07 16:37:32 +0900

    [level 0] Title: 정수 찾기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> num_list, int n) {
+    int answer = 0;
+    if(find(num_list.begin(), num_list.end(), n) == num_list.end()) {
+        return answer;
+    } else {
+        answer = 1;
+    }
+    return answer;
+}
