100dbf6ad218f5efb4d32bc171cd4220c090baf7
GangHan <40186760+StrongGwak@users.noreply.github.com>
2024-10-01 12:32:27 +0900

    [Silver II] Title: 유기농 배추, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+int M, N, K;
+
+int bfs(std::vector<std::vector<int>> map) {
+    int dx[4] = { -1, 0, 1, 0 };
+    int dy[4] = { 0, 1, 0, -1 };
+
+    int bug = 0;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (map[i][j] == 1) {
+                bug++;
+                std::queue<std::pair<int, int>> q;
+                q.push(std::make_pair(i, j));
+                map[i][j] = 0;
+                while (!q.empty()) {
+                    int x = q.front().first;
+                    int y = q.front().second;
+                    q.pop();
+
+                    for (int k = 0; k < 4; k++) {
+                        int mx = x + dx[k];
+                        int my = y + dy[k];
+                        if (mx >= 0 && mx < N && my >= 0 && my < M && map[mx][my] == 1) {
+                            q.push(std::make_pair(mx, my));
+                            map[mx][my] = 0;
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+    return bug;
+}
+
+int main()
+{
+    int TestCase;
+    std::cin >> TestCase;
+    for (int t = 0; t < TestCase; t++) {
+        std::cin >> M >> N >> K;
+        std::vector<std::vector<int>> map;
+        
+        for (int i = 0; i < N; i++) {
+            std::vector<int> tempMap;
+            std::vector<bool> tempVisited;
+            for (int j = 0; j < M; j++) {
+                tempMap.emplace_back(0);
+            }
+            map.emplace_back(tempMap);
+        }
+
+        for (int i = 0; i < K; i++) {
+            int x, y;
+            std::cin >> x >> y;
+            map[y][x] = 1;
+        }
+        std::cout << bfs(map) << '\n';
+    }
+}
