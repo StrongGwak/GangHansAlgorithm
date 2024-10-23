3bf92a83e7a6f7115674845ca087c97e0b441184
GangHan <40186760+StrongGwak@users.noreply.github.com>
2024-10-23 21:30:45 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 256 ms, Memory: 8292 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	vector<int> nums;
+
+	for (int t = 0; t < n; t++) {
+		int num;
+		cin >> num;
+		nums.emplace_back(num);
+	}
+
+	sort(nums.begin(), nums.end());
+
+	for (int i = nums.size() - 1; i >= 0; i--) {
+		cout << nums[i] << '\n';
+	}
+
+}
