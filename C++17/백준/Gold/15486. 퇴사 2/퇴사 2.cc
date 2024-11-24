4ee48ab92ac29e34a04630d1aa72be0b3e6eb74d
GangHan <40186760+StrongGwak@users.noreply.github.com>
2024-11-24 11:21:40 +0900

    [Gold V] Title: 퇴사 2, Time: 192 ms, Memory: 25460 KB -BaekjoonHub

---
+++ C++17/백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+long long dp[1500002];
+int T[1500002], P[1500002];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> T[i] >> P[i];
+	}
+
+	for (int i = N; i > 0; i--) {
+		if (i + T[i] <= N + 1) {
+			dp[i] = max(dp[i + T[i]] + P[i], dp[i + 1]);
+		}
+		else {
+			dp[i] = dp[i + 1];
+		}
+	}
+
+	cout << dp[1];
+}
